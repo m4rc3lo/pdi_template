a81eb30a03461314dd6b89fdf5c93e06f87c7b33
Marcelo Dornbusch Lopes <m4rc3lo.c@gmail.com>
2026-08-15 23:11:22 -0300

    style: format Java infrastructure helpers

--- java/src/main/java/br/univali/pdi/ExitCode.java
+++ java/src/main/java/br/univali/pdi/ExitCode.java
@@ -1,1 +1,14 @@
-package br.univali.pdi; public final class ExitCode { public static final int SUCCESS=0,GENERAL_ERROR=1,INVALID_ARGUMENTS=2,READ_ERROR=3,WRITE_ERROR=4,INVALID_PARAMETER=5,UNKNOWN_OPERATION=6; private ExitCode(){} }
+package br.univali.pdi;
+
+/** Códigos de saída padronizados entre C++, Java e Python. */
+public final class ExitCode {
+    public static final int SUCCESS = 0;
+    public static final int GENERAL_ERROR = 1;
+    public static final int INVALID_ARGUMENTS = 2;
+    public static final int READ_ERROR = 3;
+    public static final int WRITE_ERROR = 4;
+    public static final int INVALID_PARAMETER = 5;
+    public static final int UNKNOWN_OPERATION = 6;
+
+    private ExitCode() {}
+}
