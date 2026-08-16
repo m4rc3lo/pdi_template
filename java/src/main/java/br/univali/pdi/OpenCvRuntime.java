382bc2e3e54076a4caa618b25a02d6b997204787
Marcelo Dornbusch Lopes <m4rc3lo.c@gmail.com>
2026-08-15 23:11:29 -0300

    style: format Java infrastructure helpers

--- java/src/main/java/br/univali/pdi/OpenCvRuntime.java
+++ java/src/main/java/br/univali/pdi/OpenCvRuntime.java
@@ -1,1 +1,15 @@
-package br.univali.pdi; public final class OpenCvRuntime { private static boolean loaded=false; private OpenCvRuntime(){} public static synchronized void ensureLoaded(){ if(!loaded){ nu.pattern.OpenCV.loadLocally(); loaded=true; } } }
+package br.univali.pdi;
+
+/** Carrega uma única vez a biblioteca nativa do OpenCV usada pelo projeto Java. */
+public final class OpenCvRuntime {
+    private static boolean loaded = false;
+
+    private OpenCvRuntime() {}
+
+    public static synchronized void ensureLoaded() {
+        if (!loaded) {
+            nu.pattern.OpenCV.loadLocally();
+            loaded = true;
+        }
+    }
+}
